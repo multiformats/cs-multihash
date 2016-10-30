b8d3c70f1d8c3e5723229fd0d0b6b900203be3c6
Trond Bråthen <tabrath@gmail.com>
2016-10-30 12:09:51 +0100

    nuget fix

--- Multiformats.Hash.SHA3/Properties/AssemblyInfo.cs
+++ Multiformats.Hash.SHA3/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Multiformats.Hash.SHA3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Multihash sha3")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("tabrath")]
 [assembly: AssemblyProduct("Multiformats.Hash.SHA3")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © tabrath 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
