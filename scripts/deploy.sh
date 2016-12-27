ee5ac03291efcfb17ea37ea480583eb38b5973b7
Trond Bråthen <tabrath@gmail.com>
2016-12-27 15:57:10 +0100

    trying to fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 nuget pack ./Multiformats.Hash/Multiformats.Hash.csproj -IncludeReferencedProjects -Properties Configuration=Release
 nuget push ./Multiformats.Hash.$NUGET_VERSION.nupkg -ApiKey $NUGET_APIKEY
