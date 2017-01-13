b9cbafb4daeaa5d0163538b8ff0f68d1f01b2564
Trond Bråthen <tabrath@gmail.com>
2017-01-13 18:40:54 +0100

    using mef for discovery using t4 for generating

--- Multiformats.Hash/Algorithms/MultihashAlgorithm.cs
+++ Multiformats.Hash/Algorithms/MultihashAlgorithm.cs
@@ -1,8 +1,19 @@
+using System;
+using System.ComponentModel.Composition;
 using System.Threading.Tasks;
 
 namespace Multiformats.Hash.Algorithms
 {
-    public abstract class MultihashAlgorithm
+    public interface IMultihashAlgorithm
+    {
+        HashType Code { get; }
+        string Name { get; }
+        int DefaultLength { get; }
+        byte[] ComputeHash(byte[] data);
+        Task<byte[]> ComputeHashAsync(byte[] data);
+    }
+
+    public abstract class MultihashAlgorithm : IMultihashAlgorithm
     {
         public HashType Code { get; }
         public string Name { get; }
@@ -17,5 +28,29 @@ namespace Multiformats.Hash.Algorithms
 
         public abstract byte[] ComputeHash(byte[] data);
         public virtual Task<byte[]> ComputeHashAsync(byte[] data) => Task.Factory.StartNew(() => ComputeHash(data));
+    }
+
+    public interface IMultihashAlgorithmMetadata
+    {
+        HashType Code { get; }
+        string Name { get; }
+        int DefaultLength { get; }
+    }
+    
+    [AttributeUsage(AttributeTargets.Class)]
+    [MetadataAttribute]
+    public class MultihashAlgorithmExportAttribute : ExportAttribute, IMultihashAlgorithmMetadata
+    {
+        public HashType Code { get; }
+        public string Name { get; }
+        public int DefaultLength { get; }
+
+        public MultihashAlgorithmExportAttribute(HashType code, string name, int defaultLength)
+            : base(typeof(IMultihashAlgorithm))
+        {
+            Code = code;
+            Name = name;
+            DefaultLength = defaultLength;
+        }
     }
 }
---
+++ Multiformats.Hash/Algorithms/SHAKE.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace Multiformats.Hash.Algorithms
+{
+    public abstract class SHAKE : MultihashAlgorithm
+    {
+        private readonly Func<IDigest> _factory;
+
+        protected SHAKE(HashType code, string name, int defaultLength, Func<IDigest> factory)
+            : base(code, name, defaultLength)
+        {
+            _factory = factory;
+        }
+
+        public override byte[] ComputeHash(byte[] data) => _factory().ComputeHash(data);
+    }
+
+    [MultihashAlgorithmExport(HashType.SHAKE_128, "shake-128", 16)]
+    public class SHAKE_128 : SHAKE
+    {
+        public SHAKE_128()
+			: base(HashType.SHAKE_128, "shake-128", 16, () => new ShakeDigest(128))
+        {
+        }
+    }
+	    [MultihashAlgorithmExport(HashType.SHAKE_256, "shake-256", 32)]
+    public class SHAKE_256 : SHAKE
+    {
+        public SHAKE_256()
+			: base(HashType.SHAKE_256, "shake-256", 32, () => new ShakeDigest(256))
+        {
+        }
+    }
+	}
