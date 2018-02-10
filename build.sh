d21d69b2fdefc444ecdba5e0ad1a4a471b158223
Trond Arne Bråthen <tabrath@gmail.com>
2018-02-10 14:54:03 +0100

    Fix xunit path
    After update of xunit version we need to change it's path when running on travis.

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
 dotnet test ./test/Multiformats.Hash.Tests/Multiformats.Hash.Tests.csproj -c Release -f netcoreapp1.1
 dotnet build ./test/Multiformats.Hash.Tests/Multiformats.Hash.Tests.csproj -c Release -f net461
 
-mono $HOME/.nuget/packages/xunit.runner.console/2.2.0/tools/xunit.console.exe ./test/Multiformats.Hash.Tests/bin/Release/net461/Multiformats.Hash.Tests.dll
+mono $HOME/.nuget/packages/xunit.runner.console/2.3.1/tools/net452/xunit.console.exe ./test/Multiformats.Hash.Tests/bin/Release/net461/Multiformats.Hash.Tests.dll
