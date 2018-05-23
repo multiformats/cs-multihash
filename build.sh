b53bbe0284707e0f4630c06a13f26074deed8b59
Trond Arne Bråthen <tabrath@gmail.com>
2018-05-23 23:55:11 +0200

    fix: update deps and build scripts
    License: MIT
    Signed-off-by: Trond Bråthen <tabrath@gmail.com>

--- build.sh
+++ build.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/env bash
 
+mono=${MONO:=0}
+dotnet=${DOTNET:=0}
+
+case "$1" in
+  dotnet)
+    dotnet=1
+    mono=0
+    ;;
+  mono)
+    mono=1
+    dotnet=0
+    ;;
+esac
+
+echo "* settings: mono=$mono, dotnet=$dotnet"
+
 set -e
 
-if [ $TRAVIS_OS_NAME = "osx" ]; then
-  ulimit -n 1024
-  dotnet restore --disable-parallel --runtime osx-x64
-else
-  dotnet restore --runtime ubuntu-x64
+if [ $dotnet -eq 1 ]; then
+  echo "* building and testing dotnet"
+
+  if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    ulimit -n 1024
+    dotnet restore --disable-parallel --runtime osx-x64
+  else
+    dotnet restore --runtime ubuntu-x64
+  fi
+
+  dotnet test ./test/Multiformats.Hash.Tests/Multiformats.Hash.Tests.csproj -c Release -f netcoreapp2.0 --blame
 fi
 
-export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
-
-dotnet test ./test/Multiformats.Hash.Tests/Multiformats.Hash.Tests.csproj -c Release -f netcoreapp1.1
-dotnet build ./test/Multiformats.Hash.Tests/Multiformats.Hash.Tests.csproj -c Release -f net461
-
-mono $HOME/.nuget/packages/xunit.runner.console/2.3.1/tools/net452/xunit.console.exe ./test/Multiformats.Hash.Tests/bin/Release/net461/Multiformats.Hash.Tests.dll
+if [ $mono -eq 1 ]; then
+  echo "* building and testing mono"
+  export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
+  nuget restore
+  msbuild ./test/Multiformats.Hash.Tests/Multiformats.Hash.Tests.csproj /p:Configuration=Release /p:Platform=net461
+  mono $HOME/.nuget/packages/xunit.runner.console/*/tools/net452/xunit.console.exe ./test/Multiformats.Hash.Tests/bin/net461/Release/net461/Multiformats.Hash.Tests.dll
+fi
