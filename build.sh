654bf231839f31d4df5516301392764344b668c1
Trond Arne Bråthen <tabrath@gmail.com>
2018-05-24 00:38:22 +0200

    fix: more build script fixes
    License: MIT
    Signed-off-by: Trond Bråthen <tabrath@gmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 mono=${MONO:=0}
 dotnet=${DOTNET:=0}
 
+name=Multiformats.Hash.Tests
+project=./test/$name/$name.csproj
+
 case "$1" in
   dotnet)
     dotnet=1
@@ -23,18 +26,18 @@ if [ $dotnet -eq 1 ]; then
 
   if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     ulimit -n 1024
-    dotnet restore --disable-parallel --runtime osx-x64
+    dotnet restore $project --disable-parallel --runtime osx-x64
   else
-    dotnet restore --runtime ubuntu-x64
+    dotnet restore $project --runtime ubuntu-x64
   fi
 
-  dotnet test ./test/Multiformats.Hash.Tests/Multiformats.Hash.Tests.csproj -c Release -f netcoreapp2.0 --blame
+  dotnet test $project -c Release -f netcoreapp2.0 --blame
 fi
 
 if [ $mono -eq 1 ]; then
   echo "* building and testing mono"
   export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
   nuget restore
-  msbuild ./test/Multiformats.Hash.Tests/Multiformats.Hash.Tests.csproj /p:Configuration=Release /p:Platform=net461
-  mono $HOME/.nuget/packages/xunit.runner.console/*/tools/net452/xunit.console.exe ./test/Multiformats.Hash.Tests/bin/net461/Release/net461/Multiformats.Hash.Tests.dll
+  msbuild $project /p:Configuration=Release /p:Platform=net461
+  mono $HOME/.nuget/packages/xunit.runner.console/*/tools/net452/xunit.console.exe ./test/$name/bin/net461/Release/net461/$name.dll
 fi
